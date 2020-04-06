8058dc3e034e6af56e5b8cbfdeba05f9907c50f2
KNY <kny011598@gmail.com>
2020-04-06 18:05:58 +0900

    힌트 출력 메소드 추가

--- src/main/java/conerd/week1/baseball/Application.java
+++ src/main/java/conerd/week1/baseball/Application.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class Application {
     static final int MAX_ARRAY_LENGTH = 3;
     static final int DISABLE_VALUE = -1;
+    static final int GAME_RESTART = 1;
+
+    Scanner myScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         Application myApplication = new Application();
 
-        Scanner myScanner = new Scanner(System.in);
-
         boolean isGameEnded = false;
-        int gameButton = 1;
 
         while(!isGameEnded) {
             myApplication.startGame();
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
-            gameButton = myScanner.nextInt();
-            isGameEnded = (gameButton == 1) ? false : true;
+            int restartButton = myApplication.myScanner.nextInt();
+            isGameEnded = (restartButton == GAME_RESTART) ? false : true;
         }
     }
 
@@ -48,26 +48,24 @@ public class Application {
         while(!isGotRight) {
             int[] answerArray = getAnswerArray();
 
-            isGotRight = checkAnswer(answerArray, resultArray);
+            isGotRight = isRightAnswer(answerArray, resultArray);
         }
     }
 
     int[] getAnswerArray() {
-        Scanner myScanner = new Scanner(System.in);
         int answer = myScanner.nextInt();
         int[] answerArray = new int[MAX_ARRAY_LENGTH];
 
         for (int i = 0; i < MAX_ARRAY_LENGTH; i++) {
-            answerArray[i] = (int) (answer / Math.pow(10, (MAX_ARRAY_LENGTH - 1 - i)));
-            answer = (int) (answer % Math.pow(10, (MAX_ARRAY_LENGTH - 1 - i)));
+            int index = MAX_ARRAY_LENGTH - 1 - i;
+            answerArray[index] = (int) (answer % 10);
+            answer = (int) (answer / 10);
         }
 
         return answerArray;
     }
 
-    boolean checkAnswer(int[] answerArray, int[] resultArray) {
-        boolean isRightAnswer = false;
-
+    boolean isRightAnswer(int[] answerArray, int[] resultArray) {
         int strikeCount = 0;
         int ballCount = 0;
 
@@ -87,11 +85,15 @@ public class Application {
             }
         }
 
+        printHint(strikeCount, ballCount);
+
+        return (strikeCount == MAX_ARRAY_LENGTH) ? true : false;
+    }
+
+    void printHint(int strikeCount, int ballCount) {
         if (strikeCount == MAX_ARRAY_LENGTH) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            isRightAnswer = true;
         } else {
-            isRightAnswer = false;
             if (strikeCount > 0 && ballCount > 0) {
                 System.out.println(strikeCount + " 스트라이크 " + ballCount + " 볼");
             } else if (strikeCount > 0 && ballCount == 0) {
@@ -102,17 +104,5 @@ public class Application {
                 System.out.println("낫싱");
             }
         }
-
-        return isRightAnswer;
-    }
-
-    int getStrikeCount(int[] answerArray, int[] resultArray) {
-        int strikeCount = 0;
-
-        for (int i = 0; i < MAX_ARRAY_LENGTH; i++) {
-            strikeCount = (answerArray[i] == resultArray[i]) ? strikeCount + 1: strikeCount;
-        }
-
-        return strikeCount;
     }
 }
