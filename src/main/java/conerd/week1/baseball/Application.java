7358ef1cdeab588c837c81c6f34bbb6b21f52b85
KNY <kny011598@gmail.com>
2020-04-06 15:21:35 +0900

    난수 설정

---
+++ src/main/java/conerd/week1/baseball/Application.java
@@ -0,0 +1,28 @@
+package conerd.week1.baseball;
+
+public class Application {
+    static final int MAX_ARRAY_LENGTH = 3;
+
+    public static void main(String[] args) {
+        Application myApplication = new Application();
+
+        myApplication.startGame();
+    }
+
+    void startGame() {
+        int[] resultArray = new int[MAX_ARRAY_LENGTH];
+        int[] answerArray = new int[MAX_ARRAY_LENGTH];
+
+        resultArray = getRandomArray();
+
+    }
+
+    int[] getRandomArray() {
+        int[] randomArray = new int[MAX_ARRAY_LENGTH];
+        for (int i = 0; i < MAX_ARRAY_LENGTH; i++) {
+            randomArray[i] = (int) (Math.random() * 10);
+        }
+
+        return randomArray;
+    }
+}
