9905bfa00b7c1f3f5411a18f96a7319264e82a77
KNY <kny011598@gmail.com>
2020-04-09 00:38:07 +0900

    strike, ball 카운트 로직 리팩토링

--- src/main/java/conerd/week1/baseball/Application.java
+++ src/main/java/conerd/week1/baseball/Application.java
@@ -6,8 +6,12 @@ public class Application {
     static final int MAX_ARRAY_LENGTH = 3;
     static final int DISABLE_VALUE = -1;
     static final int GAME_RESTART = 1;
+    static final int GAME_END = 2;
 
     Scanner myScanner = new Scanner(System.in);
+
+    int[] resultArray = new int[MAX_ARRAY_LENGTH];
+    int[] answerArray = new int[MAX_ARRAY_LENGTH];
 
     public static void main(String[] args) {
         Application myApplication = new Application();
@@ -17,24 +21,38 @@ public class Application {
         while(!isGameEnded) {
             myApplication.startGame();
 
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
-            int restartButton = myApplication.myScanner.nextInt();
-            isGameEnded = (restartButton == GAME_RESTART) ? false : true;
+            isGameEnded = myApplication.isGameEnd();
         }
     }
 
-    void startGame() {
-        System.out.println("시작");
-        int[] resultArray = getRandomArray();
-        for(int i = 0; i < MAX_ARRAY_LENGTH; i++) {
-            System.out.println(resultArray[i]);
+    boolean isGameEnd() {
+        boolean isGameEnded = false;
+        int restartButton = DISABLE_VALUE;
+
+        while (restartButton == DISABLE_VALUE) {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
+            restartButton = myScanner.nextInt();
+
+            if (restartButton != GAME_RESTART && restartButton != GAME_END) {
+                System.out.println("요청에 맞지 않는 답입니다. 다시 입력하세요.");
+                restartButton = DISABLE_VALUE;
+            } else {
+                isGameEnded = (restartButton == GAME_RESTART) ? false : true;
+            }
         }
+
+        return isGameEnded;
+    }
+
+    void startGame() {
+        resultArray = getRandomArray();
 
         playGame(resultArray);
     }
 
     int[] getRandomArray() {
         int[] randomArray = new int[MAX_ARRAY_LENGTH];
+
         for (int i = 0; i < MAX_ARRAY_LENGTH; i++) {
             randomArray[i] = (int) (Math.random() * 10);
         }
@@ -46,26 +64,23 @@ public class Application {
         boolean isGotRight = false;
 
         while(!isGotRight) {
-            int[] answerArray = getAnswerArray();
+            setAnswerArray();
 
-            isGotRight = isRightAnswer(answerArray, resultArray);
+            isGotRight = isRightAnswer();
         }
     }
 
-    int[] getAnswerArray() {
+    void setAnswerArray() {
         int answer = myScanner.nextInt();
-        int[] answerArray = new int[MAX_ARRAY_LENGTH];
 
         for (int i = 0; i < MAX_ARRAY_LENGTH; i++) {
             int index = MAX_ARRAY_LENGTH - 1 - i;
             answerArray[index] = (int) (answer % 10);
             answer = (int) (answer / 10);
         }
-
-        return answerArray;
     }
 
-    boolean isRightAnswer(int[] answerArray, int[] resultArray) {
+    boolean isRightAnswer() {
         int strikeCount = 0;
         int ballCount = 0;
 
@@ -74,35 +89,43 @@ public class Application {
                 strikeCount += 1;
                 answerArray[i] = DISABLE_VALUE;
             } else {
-                boolean isDone = false;
-                for (int j = 0; j < MAX_ARRAY_LENGTH && !isDone; j++) {
-                    if (resultArray[i] == answerArray[j]) {
-                        ballCount += 1;
-                        answerArray[j] = DISABLE_VALUE;
-                        isDone = true;
-                    }
-                }
+                ballCount = (isBall(resultArray[i])) ? ballCount + 1 : ballCount;
             }
         }
 
-        printHint(strikeCount, ballCount);
+        if (strikeCount == MAX_ARRAY_LENGTH) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        } else {
+            printHint(strikeCount, ballCount);
+        }
 
         return (strikeCount == MAX_ARRAY_LENGTH) ? true : false;
     }
 
     void printHint(int strikeCount, int ballCount) {
-        if (strikeCount == MAX_ARRAY_LENGTH) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.println("낫싱");
         } else {
-            if (strikeCount > 0 && ballCount > 0) {
-                System.out.println(strikeCount + " 스트라이크 " + ballCount + " 볼");
-            } else if (strikeCount > 0 && ballCount == 0) {
-                System.out.println(strikeCount + " 스트라이크");
-            } else if (strikeCount == 0 && ballCount > 0) {
-                System.out.println(ballCount + " 볼");
-            } else {
-                System.out.println("낫싱");
+            if (strikeCount > 0) {
+                System.out.printf("%d 스트라이크 ", strikeCount);
+            }
+            if (ballCount > 0) {
+                System.out.printf("%d 볼", ballCount);
+            }
+            System.out.println();
+        }
+    }
+
+    boolean isBall(int result) {
+        boolean isBall = false;
+
+        for (int j = 0; j < MAX_ARRAY_LENGTH && !isBall; j++) {
+            if (result == answerArray[j]) {
+                answerArray[j] = DISABLE_VALUE;
+                isBall = true;
             }
         }
+
+        return isBall;
     }
 }
